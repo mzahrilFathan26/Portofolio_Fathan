5df5e19550ffeb43dca4f24fa7e67da0538e7f57
muhamadirpan266 <fotopribadi56@gmail.com>
2025-11-04 09:54:14 +0700

    update

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const navToggle = document.getElementById('nav-toggle');
 const navMenu = document.getElementById('nav-menu');
 
+// Toggle menu dan animasi silang
 navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
-  navToggle.classList.toggle('active'); // untuk animasi silang
+  navToggle.classList.toggle('active');
 });
 
-
-
+// Tutup menu jika klik di luar navbar atau toggle
+document.addEventListener('click', (e) => {
+  if (
+    navMenu.classList.contains('active') && // hanya jika menu aktif
+    !navMenu.contains(e.target) && // klik bukan di dalam navMenu
+    !navToggle.contains(e.target) // klik bukan di toggle button
+  ) {
+    navMenu.classList.remove('active');
+    navToggle.classList.remove('active');
+  }
+});
 
 // Scroll to top button
 const scrollTopBtn = document.getElementById('scrollTopBtn');
@@ -36,8 +46,11 @@ document.querySelectorAll('.nav ul li a').forEach(link => {
         // Tutup menu mobile jika aktif
         if(navMenu.classList.contains('active')) {
           navMenu.classList.remove('active');
+          navToggle.classList.remove('active');
         }
       }
     }
   });
 });
+
+
